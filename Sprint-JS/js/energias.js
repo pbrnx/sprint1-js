d586f6ac4449df3a8e83547bf8504560385ee76e
pbrnx <128100284+pbrnx@users.noreply.github.com>
2023-04-13 11:32:04 -0300

    Add files via upload

--- Sprint-JS/js/energias.js
+++ Sprint-JS/js/energias.js
@@ -9,6 +9,20 @@ image.addEventListener('mouseout', () => {
   image.style.width = '500px';
 });
 
+function mostrarResultado() {
+  // Encontra o radio button selecionado pelo usuário
+  const opcaoSelecionada = document.querySelector('input[name="opcao"]:checked');
+
+  // Verifica se o usuário selecionou alguma opção
+  if (opcaoSelecionada) {
+    // Obtém o valor selecionado pelo usuário
+    const valorSelecionado = opcaoSelecionada.value;
+    // Mostra o resultado na página
+    alert("Obrigado por votar!")
+  } else {
+    alert('Por favor, selecione uma opção');
+  }
+}
 
 
 
